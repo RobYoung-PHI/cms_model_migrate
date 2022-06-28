1692214b590ebefa31368671b54c7f9d3de2b617
RobYoung-PHI <59060776+RobYoung-PHI@users.noreply.github.com>
2022-06-28 18:30:34 +0100

    First commit of JS

---
+++ public/js/cms_model_migrate.js
@@ -0,0 +1,21 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+async function fetchAsync () {
+  let response = await fetch('https://i70roqwrjj.execute-api.us-east-1.amazonaws.com/main/getgv?user_id='+getParameterByName("user_id"));
+  let data = await response.json();
+  return data;
+}
+
+fetchAsync()
+    .then(
+      console.log(data)
+    )
+    .catch(reason => console.log(reason.message))
