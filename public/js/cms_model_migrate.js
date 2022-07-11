47c704937043c806c1504edb0ecb93a8f2ff50bc
RobYoung-PHI <59060776+RobYoung-PHI@users.noreply.github.com>
2022-07-11 18:06:19 +0100

    Add modelcheck API

--- public/js/cms_model_migrate.js
+++ public/js/cms_model_migrate.js
@@ -8,21 +8,30 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+// response = await fetch('https://i70roqwrjj.execute-api.us-east-1.amazonaws.com/main/getgv?user_id='+getParameterByName("user_id"), { mode: 'cors', headers: { 'x-api-key': 'ihl9Ec68TX4asxR619oHaBbC1YaBAiu3Lnd5Rh63', 'Access-Control-Allow-Origin': '*', 'Accept': '*/*' } } )
+//    , { mode: 'cors', headers: { 'x-api-key': '5485748746547e847483983343433243', 'User-Agent' : 'My-App', 'Accept': '*/*'}}
+
 async function fetchAsync () {
-  let response = {}
-  try {
-    response = await fetch('https://i70roqwrjj.execute-api.us-east-1.amazonaws.com/main/getgv?user_id='+getParameterByName("user_id"), { mode: 'cors', headers: { 'x-api-key': 'ihl9Ec68TX4asxR619oHaBbC1YaBAiu3Lnd5Rh63', 'Access-Control-Allow-Origin': '*', 'Accept': '*/*' } } )
-    //    , { mode: 'cors', headers: { 'x-api-key': '5485748746547e847483983343433243', 'User-Agent' : 'My-App', 'Accept': '*/*'}}
-  } catch (err) {
-    response = err
-  }
-  // let data = await response.json();
-  let data = response
+  let response = await fetch('https://z0kb9cr2ed.execute-api.us-east-1.amazonaws.com/main/modelcheck/'+getParameterByName("uid"), 
+        method: 'GET',
+        headers: {
+            'X-API-KEY': 'MxomFM7F8N74wG5Sqkp397eeJkdNXHGTbhZihhxa',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        //body: JSON.stringify(callData)
+    });
+  let data = await response.json();
   return data;
 }
 
 fetchAsync()
     .then(
-      console.log(data)
+      data => {
+        for (let i = 0; i < data.Items.length; i++) {
+          console.log(data.Items[i])
+          //document.getElementById("tile"+(i+1)).src = 'img/Scrabble_Tile_'+data.Items[0].tileRack.charAt(i)+'.jpg'
+        }
+      }
     )
     .catch(reason => console.log(reason.message))
