6ef4a63057e4f171636d4333713d697597edfd9e
RobYoung-PHI <59060776+RobYoung-PHI@users.noreply.github.com>
2022-07-15 17:33:29 +0100

    Initial commit

---
+++ lambda/analytics_cms_model_migrate_get_payload.py
@@ -0,0 +1,30 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+import datetime
+import json
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    dynamo_table = dynamodb.Table('analytics_cms_check_results')
+
+    item = {}
+    item_hash = str(event['source_id']) + '_' + str(event['assoc_id']) + '_' + str(event['uid'])
+    item['user_id'] = str(event['uid'])
+    item['check_id'] = item_hash
+    item['assoc_id'] = str(event['assoc_id'])
+    item['source_id'] = str(event['source_id'])
+    item['status'] = str(event['status'])
+    item['updated_at'] = datetime.datetime.now().strftime("%Y-%m-%d"+"T"+"%H:%M:%S.%f"+"Z")
+
+    try:
+        dynamo_table.put_item(Item=item, ConditionExpression='attribute_not_exists(itemhash)')
+        response = 'Success'
+    except Exception as e:
+        response = 'Failure' + str(e)
+
+    return {
+        'statusCode': 200,
+        'headers': {'Access-Control-Allow-Origin': '*' },
+        'body': response
+    }
