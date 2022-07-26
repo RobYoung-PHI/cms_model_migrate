b040b607e77dc0f32ef323dc29263c30e7b77826
RobYoung-PHI <rob@roeburn.com>
2022-07-26 11:27:17 +0100

    add TSV download functionality

--- public/results/js/cms_results.js
+++ public/results/js/cms_results.js
@@ -57,6 +57,35 @@ function generateTable(table, data) {
   }
 }
 
+function download_table_as_tsv(table_id, separator = '\t') {
+    let rows = document.querySelectorAll('table#' + table_id + ' tr');
+    let tsv = [];
+    for (let i = 0; i < rows.length; i++) {
+        let row = [], cols = rows[i].querySelectorAll('td, th');
+        for (let j = 0; j < cols.length; j++) {
+            let data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+            data = data.replace(/"/g, '""');
+            row.push(data);
+        }
+        tsv.push(row.join(separator));
+    }
+    let tsv_string = tsv.join('\n');
+    let filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.tsv';
+    let link = document.createElement('a');
+    link.style.display = 'none';
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(tsv_string));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+const download_btn = document.getElementById('download_btn');
+download_btn.addEventListener('click', function() {
+  download_table_as_tsv('tablemain')
+});
+
 async function fetchAsync () {
   let response = await fetch('https://z0kb9cr2ed.execute-api.us-east-1.amazonaws.com/main/resultscheck/', {
         method: 'GET',
